03e10f00b0b016a64b08e5c1e65463b1d8118ddd
Alex Gustafsson <alex-gustafsson@live.se>
2021-08-16 14:06:07 +0200

    lsp: add lsp library

---
+++ lsp/handler.go
@@ -0,0 +1,73 @@
+package lsp
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/sourcegraph/go-lsp"
+	"github.com/sourcegraph/jsonrpc2"
+)
+
+type Handler struct{}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (handler Handler) Handle(ctx context.Context, connection *jsonrpc2.Conn, request *jsonrpc2.Request) {
+	log.Info("request", request)
+	response, err := handler.handle(ctx, connection, request)
+	if err != nil {
+		log.Error("error creating response", err)
+		return
+	}
+	log.Info("response", response)
+	err = connection.Reply(ctx, request.ID, response)
+	if err != nil {
+		log.Error("error sending response", err)
+	}
+}
+
+// https://github.com/a-h/qt-lsp/blob/6c6909dc8457bdd14ee3c80513c0cc87394c6aa7/handler.go#L18
+func (handler Handler) handle(ctx context.Context, connection *jsonrpc2.Conn, request *jsonrpc2.Request) (interface{}, error) {
+	switch request.Method {
+	case "initialize":
+		if request.Params == nil {
+			return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
+		}
+
+		kind := lsp.TDSKIncremental
+		return lsp.InitializeResult{
+			Capabilities: lsp.ServerCapabilities{
+				TextDocumentSync: &lsp.TextDocumentSyncOptionsOrKind{
+					Kind: &kind,
+				},
+				CompletionProvider:     &lsp.CompletionOptions{ResolveProvider: true, TriggerCharacters: []string{"(", "."}},
+				DefinitionProvider:     true,
+				TypeDefinitionProvider: true,
+				DocumentSymbolProvider: true,
+				HoverProvider:          true,
+				ReferencesProvider:     true,
+				ImplementationProvider: true,
+				SignatureHelpProvider:  &lsp.SignatureHelpOptions{TriggerCharacters: []string{"(", ","}},
+			},
+		}, nil
+	case "initialized":
+		// A notification that the client is ready to receive requests. Ignore
+		return nil, nil
+	case "textDocument/hover":
+		if request.Params == nil {
+			return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
+		}
+		var params lsp.TextDocumentPositionParams
+		if err := json.Unmarshal(*request.Params, &params); err != nil {
+			return nil, err
+		}
+		log.Infof("Hovering %d %d", params.Position.Line, params.Position.Character)
+		return nil, nil
+	}
+
+	return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeMethodNotFound, Message: fmt.Sprintf("method not supported: %s", request.Method)}
+}

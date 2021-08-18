564cfbf74e407fc2677b9e82648650f6ceca1480
Alex Gustafsson <alex-gustafsson@live.se>
2021-08-18 18:51:10 +0200

    vscode: make bagels command configurable

--- tools/vscode/client/src/extension.ts
+++ tools/vscode/client/src/extension.ts
@@ -1,7 +1,8 @@
 import * as path from "path";
 import {
   ExtensionContext,
-  languages
+  languages,
+  workspace
 } from "vscode";
 
 import {
@@ -16,13 +17,14 @@ let client: LanguageClient;
 export function activate(context: ExtensionContext) {
   languages.setLanguageConfiguration("bake", { wordPattern: /[a-zA-Z_]\w*/ });
 
-  let serverModule = "bagels";
+  const config = workspace.getConfiguration("bagels");
+  const bagelsCommand = config.get<string>("command")!
 
   // If the extension is launched in debug mode then the debug server options are used
   // Otherwise the run options are used
   let serverOptions: ServerOptions = {
-    run: {command: serverModule, transport: TransportKind.stdio},
-    debug: {command: serverModule, transport: TransportKind.stdio},
+    run: {command: bagelsCommand, transport: TransportKind.stdio},
+    debug: {command: bagelsCommand, transport: TransportKind.stdio},
   };
 
   let clientOptions: LanguageClientOptions = {

7493f8adcb672e70b669428d9e4c1f44e3d9b563
Alex Gustafsson <alex-gustafsson@live.se>
2021-08-21 10:30:18 +0200

    parsing: don't show arguments in parse tree if there are none

--- parsing/nodes/invokation.go
+++ parsing/nodes/invokation.go
@@ -43,11 +43,13 @@ func (node *Invokation) DotString() string {
 	var builder strings.Builder
 	fmt.Fprintf(&builder, "\"%p\" [label=\"%s\"];\n", node, "invocation")
 	fmt.Fprintf(&builder, "\"%p\" -> \"%p\" [label=\"%s\"];\n", node, node.Operand, "operand")
-	fmt.Fprintf(&builder, "\"%p\" -> \"%p\";\n", node, node.Arguments)
-	fmt.Fprintf(&builder, "\"%p\" [label=\"arguments\"];\n", node.Arguments)
-	for i, argument := range node.Arguments {
-		fmt.Fprintf(&builder, "\"%p\" -> \"%p%d\"\n", node.Arguments, &argument, i)
-		fmt.Fprintf(&builder, "\"%p%d\" [label=\"%s\"];\n", &argument, i, argument)
+	if len(node.Arguments) > 0 {
+		fmt.Fprintf(&builder, "\"%p\" -> \"%p\";\n", node, node.Arguments)
+		fmt.Fprintf(&builder, "\"%p\" [label=\"arguments\"];\n", node.Arguments)
+		for i, argument := range node.Arguments {
+			fmt.Fprintf(&builder, "\"%p\" -> \"%p%d\"\n", node.Arguments, &argument, i)
+			fmt.Fprintf(&builder, "\"%p%d\" [label=\"%s\"];\n", &argument, i, argument)
+		}
 	}
 	builder.WriteString(node.Operand.DotString())
 	return builder.String()

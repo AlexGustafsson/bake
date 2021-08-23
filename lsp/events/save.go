9a420f9e1144da58319dae69b3417325ec550cac
Alex Gustafsson <alex-gustafsson@live.se>
2021-08-23 13:35:43 +0200

    lsp: validate semantics

--- lsp/events/save.go
+++ lsp/events/save.go
@@ -4,18 +4,41 @@ import (
 	"github.com/AlexGustafsson/bake/ast"
 	"github.com/AlexGustafsson/bake/lsp/state"
 	"github.com/AlexGustafsson/bake/parsing"
+	"github.com/AlexGustafsson/bake/semantics"
 	"github.com/sourcegraph/go-lsp"
 )
 
 func HandleSave(event *Event, document *state.Document) {
 	document.ClearDiagnostics()
 
-	_, err := parsing.Parse(document.Content)
+	source, err := parsing.Parse(document.Content)
 	if err != nil {
 		if treeError, ok := err.(*ast.TreeError); ok {
 			document.CreateDiagnostic(lsp.Error, *treeError.Range, treeError.Message)
 		} else {
 			document.PublishError(event.Connection, event.Context, err)
+		}
+	}
+
+	sourceScope, errs := semantics.Build(source)
+	if len(errs) > 0 {
+		for _, err := range errs {
+			if treeError, ok := err.(*ast.TreeError); ok {
+				document.CreateDiagnostic(lsp.Error, *treeError.Range, treeError.Message)
+			} else {
+				document.PublishError(event.Connection, event.Context, err)
+			}
+		}
+	}
+
+	errs = semantics.Validate(source, sourceScope)
+	if len(errs) > 0 {
+		for _, err := range errs {
+			if treeError, ok := err.(*ast.TreeError); ok {
+				document.CreateDiagnostic(lsp.Error, *treeError.Range, treeError.Message)
+			} else {
+				document.PublishError(event.Connection, event.Context, err)
+			}
 		}
 	}
 
